41cec4af6457d0c8205a7462504bd62771a5131a
cLaudiamacrini <157354530+cLaudiamacrini@users.noreply.github.com>
2024-01-25 15:42:00 +0000

    !!

--- doc23.c
+++ doc23.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
-int main() 
+#include <math.h>
+int main()
 { 
+    float a;
+    float b;
+    float c;
+    printf("inserisci primo valore");
+    scanf("%f", &a);
+    printf("inserisci secondo valore");
+    scanf("%f", &b);
+    printf("inserisci terzo valore");
+    scanf("%f", &c); 
+    printf("Radice di %2f, vale: %2f",a, sqrt(a));
     
+}
